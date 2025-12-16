b17766fad5ad87cc92585c0d0679489c92ef6f21
MANEESHGULLA <maneesh2045@gmail.com>
2025-12-16 21:20:12 +0530

    Update SGDRegressor.py

--- SGDRegressor.py
+++ SGDRegressor.py
@@ -1,32 +1,3 @@
-# ======================== Example 1: SGDRegressor on synthetic linear data ========================
-
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import SGDRegressor
-from sklearn.metrics import mean_squared_error,r2_score
-import matplotlib.pyplot as plt
-
-np.random.seed(42)
-x=2*np.random.rand(100,1)
-y=3*x.flatten()+2+np.random.randn(100)
-
-x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=42)
-
-sgd=SGDRegressor(max_iter=1000,tol=1e-3,eta0=0.1,learning_rate="constant",random_state=42)
-sgd.fit(x_train,y_train)
-y_pred=sgd.predict(x_test)
-print("\ny_pred")
-print(y_pred)
-
-print("\nslope:",sgd.coef_[0])
-print("intercept:",sgd.intercept_[0])
-print("mean square error:",mean_squared_error(y_test,y_pred))
-print("r2_score:",r2_score(y_test,y_pred))
-
-plt.scatter(x,y,color="blue",label="data")
-plt.plot(x_test,y_pred,color="red",label="regression line")
-plt.legend()
-plt.show()
 
 # ======================== Example 2: SGDRegressor on California Housing dataset ========================
 
